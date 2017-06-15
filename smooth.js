4b7888b9c65470a01812570de455b9723fd419ad
WalterAngelo <walterangelodasmarinas@gmail.com>
2017-06-15 14:00:54 +0800

    Create smooth.js

--- smooth.js
+++ smooth.js
@@ -34,3 +34,12 @@ $('a[href*="#"]')
       }
     }
   });
+
+var timeOut;
+function scrollToTop() {
+  if (document.body.scrollTop!=0 || document.documentElement.scrollTop!=0){
+    window.scrollBy(0,-50);
+    timeOut=setTimeout('scrollToTop()',10);
+  }
+  else clearTimeout(timeOut);
+}
